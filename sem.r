4a02152374907dff78f87e0f074f18f70be64db2
Arian Jafari <arian91@hotmail.com>
2018-01-23 15:06:29 +0100

    Added simple plot drawing.

--- sem.r
+++ sem.r
@@ -1,6 +1,7 @@
 ### Inlämningsuppgift Strukturekvationsmodeller
 library(lavaan)
 library(sem)
+library(semPlot)
 
 data_path <- ("C:\\Users\\Arian\\Documents\\R\\psych-stats\\SEM_Attachment_Depression.txt")
 data <- read.table(data_path, header = TRUE)
@@ -18,7 +19,7 @@ model <- '
   # regressionseffekter
   dependence ~ attach_anxiety + attach_avoid
   self_criticism ~ attach_anxiety + attach_avoid
-  depression ~ attach_anxiety + attach_avoid + dependence + self_criticism
+  depression ~ attach_avoid + dependence + self_criticism
 
   # korrelationer
   attach_anxiety ~~ attach_avoid
@@ -27,3 +28,7 @@ model <- '
 model_fit <- sem(model, data = data)
 parameterEstimates(model_fit, standardized = TRUE)
 summary(model_fit, standardized = TRUE)
+
+# Rita upp en enkel graf
+# För vidare graftutorial, kolla https://rstudio-pubs-static.s3.amazonaws.com/78926_5aa94ae32fae49f3a384ce885744ef4a.html
+semPaths(model_fit, intercept = FALSE, whatLabel = "est", residuals = FALSE, exoCov = FALSE)
