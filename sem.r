6ddef91918031c554ded80ffdd02f1a14db4aaa4
Arian Jafari <arian91@hotmail.com>
2018-01-23 14:38:51 +0100

    Removed unnecessary regression that wasn't allowing model to converge. Basic SEM model now working.

--- sem.r
+++ sem.r
@@ -9,20 +9,21 @@ names(data)
 
 model <- '
   # mätmodellen
-  attachment_anxiety =~ Anxiety_1 + Anxiety_2 + Anxiety_3
-  attachment_avoidance =~ Avoid_1 + Avoid_2 + Avoid_3
+  attach_anxiety =~ Anxiety_1 + Anxiety_2 + Anxiety_3
+  attach_avoid =~ Avoid_1 + Avoid_2 + Avoid_3
   dependence =~ DEQ_D + PSI_D
   self_criticism =~ DEQ_SC + PSI_SC
-  depressive_symptoms =~ SDS + CES_D + DASS_D
+  depression =~ SDS + CES_D + DASS_D
   
   # regressionseffekter
-  dependence ~ attachment_anxiety + attachment_avoidance
-  self_criticism ~ attachment_anxiety + attachment_avoidance
-  depressive_symptoms ~ attachment_anxiety + attachment_avoidance + dependence + self_criticism
+  dependence ~ attach_anxiety + attach_avoid
+  self_criticism ~ attach_anxiety + attach_avoid
+  depression ~ attach_anxiety + attach_avoid + dependence + self_criticism
 
   # korrelationer
-  attachment_anxiety ~~ attachment_avoidance
+  attach_anxiety ~~ attach_avoid
 '
 
 model_fit <- sem(model, data = data)
-summary(model_fit)
+parameterEstimates(model_fit, standardized = TRUE)
+summary(model_fit, standardized = TRUE)
