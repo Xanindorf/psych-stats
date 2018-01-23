00fca0ce331ef16a95b72b9dcd31a1e742e4f86c
Arian Jafari <arian91@hotmail.com>
2018-01-23 13:51:28 +0100

    Adding R code file.

---
+++ sem.r
@@ -0,0 +1,6 @@
+### Inlämningsuppgift Strukturekvationsmodeller
+
+data_path <- ("C:\\Users\\Arian\\Documents\\R\\psych-stats\\SEM_Attachment_Depression.txt")
+data <- read.table(data_path, header = TRUE)
+
+names(data)
